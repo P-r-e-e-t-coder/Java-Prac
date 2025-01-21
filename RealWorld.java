a657578ec2df24db5db02bb6778314d91a032faf
P-r-e-e-t-coder <patel.preet@atharvaits.com>
2025-01-21 15:02:23 +0530

    example 1

---
+++ RealWorld.java
@@ -0,0 +1,17 @@
+class RealWorld {
+	public static void main(String[] args) {
+		System.out.println("Real world objects:-");
+	}
+}
+
+class Book {
+	int page = 1;
+
+	void nextpage() {
+		page++;
+	}
+
+	void prevpage() {
+		page--;
+	}
+}
