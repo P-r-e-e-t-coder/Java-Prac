bfcccd4001b2ee8b79d3bf8189d3816c73ea4cbb
P-r-e-e-t-coder <patel.preet@atharvaits.com>
2025-01-22 16:31:59 +0530

    Example-2

---
+++ MultiArrayDemo.java
@@ -0,0 +1,10 @@
+class MultiArrayDemo {
+    public static void main(String[] args) {
+        String[][] names = {
+            {"Mr. ", "Mrs. ", "Ms. "},
+            {"Preet", "Jones"}
+        };
+        System.out.println(names[0][0] + names[1][0]);
+        System.out.println(names[0][2] + names[1][1]);
+    }
+}
